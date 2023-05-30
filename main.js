e0fcf4683c0dc0cb5047243fcadf5c04f490662a
JoshOpenform <106382678+JoshOpenform@users.noreply.github.com>
2023-05-30 15:42:36 +0200

    Path Test

--- main.js
+++ main.js
@@ -270,7 +270,7 @@ class Banner {
           continue;
         }
         document.cookie =
-          cookieName + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+          cookieName + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/cookieCDN;";
       }
     } catch (e) {}
   }
