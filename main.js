f156e77b3ec72f5e6fbec1b103d12ca5eac7f202
PeterOpenform <64835024+PeterOpenform@users.noreply.github.com>
2023-08-29 10:34:05 +0200

    Update main.js
    change ofc to cookie button

--- main.js
+++ main.js
@@ -99,13 +99,13 @@ class Banner {
     <div class='ofc-message-container'>
         <p>By clicking "Accept All Cookies", you agree to the storing of cookies on your device to enhance site navigation, analyse site usage, and assist in our marketing efforts.</p>
     </div>
-    <div class='ofc-button-container'>
-        <button data-item='js-settings-button' type='button' class='ofc-button'>Cookie Settings</button>
-        <button data-item='js-reject-button' type='button' class='ofc-button'>Reject All</button>
-        <button data-item='js-accept-button' type='button' class='ofc-button'>Accept All</button>
+    <div class='cookie-button-container'>
+        <button data-item='js-settings-button' type='button' class='cookie-button'>Cookie Settings</button>
+        <button data-item='js-reject-button' type='button' class='cookie-button'>Reject All</button>
+        <button data-item='js-accept-button' type='button' class='cookie-button'>Accept All</button>
     </div>
     <div class='ofc-close-container'>
-        <button class='ofc-button ofc-close' data-item='js-close-button'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16"> <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/> </svg></button>
+        <button class='cookie-button ofc-close' data-item='js-close-button'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16"> <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/> </svg></button>
     </div>
     `;
 
